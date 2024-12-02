b8f994c9ecee13a66a6639b6ba54ecc726a29569
ronguyen1998 <ronguyen1998@gmail.com>
2024-12-02 13:36:13 +0700

    Setup Redux to Project

---
+++ src/store/reducers/rootReducers.js
@@ -0,0 +1,13 @@
+const initState = {
+    users: [
+        { id: 1, name: 'Ro' },
+        { id: 2, name: 'abc' },
+    ]
+}
+
+const rootReducer = (state = initState, action) => {
+
+    return state
+}
+
+export default rootReducer;
